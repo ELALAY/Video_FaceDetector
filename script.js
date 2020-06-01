b8f29930709467bb26c9a1f23f9b58d0342c7da4
Aymane97 <elalamiaymane97@gmail.com>
2020-06-01 09:52:32 +0100

    resizing and matching the face canvas

--- script.js
+++ script.js
@@ -20,11 +20,14 @@ video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
     document.body.append(canvas)
     const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () => {
         const detections = await faceapi.detecAllFaces(video, 
             new faceapi.TinyFaceDetectorOptions())
                 .withFaceLandmarks().withFaceExpressions()
         console.log(detections)
         const resizeDetections = faceapi.resisedResults(detections.displaySize)
+        canvas.getContext('2D').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDeections(canvas, resizeDetections)
     }, 100)
 })
