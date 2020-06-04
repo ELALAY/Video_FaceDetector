c3708c59abaf1718798e0216afdfdca43da1b123
Aymane97 <elalamiaymane97@gmail.com>
2020-06-04 07:33:40 +0100

    rewriting the functions, added the canvas again

--- script.js
+++ script.js
@@ -2,40 +2,35 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo() )
 
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
 }
 
 video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
+    
     document.body.append(canvas)
-    const displaySize = { width: video.width, height: video.height }
-    faceapi.matchDimensions(canvas, displaySize)
+    const displaySize = {width: video.width, height: video.height}
+
     setInterval(async () => {
-        const detections = await faceapi.detecAllFaces(video, 
-            new faceapi.TinyFaceDetectorOptions())
-                .withFaceLandmarks().withFaceExpressions()
+        const detections = await faceapi.detectAllFaces(video, 
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
         console.log(detections)
-        const resizeDetections = faceapi.resisedResults(detections.displaySize)
-        canvas.getContext('2D').clearRect(0, 0, canvas.width, canvas.height)
         
-        //drawing a rectangle over the face
-        faceapi.draw.drawDeections(canvas, resizeDetections)
-        
-        //drawing dots on the face lanmarks (mouth, eyes ...)
-        faceapi.draw.drawFaceLandmarks(canvas, resizeDetections)
-        
-        //making face expressions
-        faceapi.draw.drawFaceExpressions(canvas, resizeDetections)
-    }, 100)
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+    }, 100);
+  
+    
 })
